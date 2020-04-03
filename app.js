11398759d45d1bde2a41fccb1593d5b14118b8c5
rafaella nicoletti <rafaella_np@hotmail.com>
2020-04-03 20:16:47 -0300

    fade right

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ $(document).scroll(function () {
 
 function windowScroll() {
     var st = $(document).scrollTop();
+    console.log(st)
 
-    $("#aff").css({ "top": 90- st * 0.05 + "px" });
-    $("#aff").css({ "left": 80 - st * 0.15 + "px" });
-    
+    if (st < 1200){
+        $("#aff").css({ "top": 90 - st * 0.05 + "px" });
+        $("#aff").css({ "left": 80 - st * 0.15 + "px" });
+        $("#services").css({ "top": 40 + st * 0.05 + "px" });
+        $("#services").css({ "left": 10 + st * 0.15 + "px" });
+    }
+
+
 };
