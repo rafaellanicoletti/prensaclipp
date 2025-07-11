df7ba9366c168f2eabe82f065c70748379396639
rafaella nicoletti <rafaella_np@hotmail.com>
2025-07-11 16:36:40 -0400

    changes

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-$(document).scroll(function () {
-    windowScroll()
-});
-
-function windowScroll() {
-    var st = $(document).scrollTop();
-    console.log(st)
-
-    if (st < 1200){
-        $("#aff").css({ "top": 90 - st * 0.05 + "px" });
-        $("#aff").css({ "left": 80 - st * 0.15 + "px" });
-        $("#services").css({ "top": 40 + st * 0.05 + "px" });
-        $("#services").css({ "left": 10 + st * 0.15 + "px" });
-        
+$(document).ready(function() {
+    $(document).scroll(function () {
+        windowScroll();
+    });
+  
+    function windowScroll() {
+        var st = $(document).scrollTop();
+        var screenWidth = $(window).width();
+  
+        if (screenWidth < 768) {
+            $("#aff").css({ left: "0px" });
+            $("#services").css({ left: "0px" });
+            return;
+        }
+  
+        if (st < 1200) {
+            $("#aff").css({ 
+                left: (80 - st * 0.15) + "px"
+            });
+  
+            $("#services").css({ 
+                left: (10 + st * 0.15) + "px"
+            });
+        }
     }
-};
+  });
+  console.log("Screen width:", screenWidth);  
 
+document.querySelectorAll('.navbar-nav a').forEach(function(navLink) {
+    navLink.addEventListener('click', function () {
+      var navbarCollapse = document.querySelector('.navbar-collapse');
+      if (navbarCollapse.classList.contains('show')) {
+        var bsCollapse = new bootstrap.Collapse(navbarCollapse, {toggle: false});
+        bsCollapse.hide();
+      }
+    });
+  });
 
+  $('#carouselExampleControls').carousel({
+    interval: 2000
+  });
